be1aa43ece6e79bbb43ff8d4b6173c7869617c2a
macoteianneresearch <macoteiannebernadettesedon@gmail.com>
2025-09-25 19:38:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showWarning() {
+  document.getElementById("warningPopup").classList.remove("hidden");
+}
+function closeWarning() {
+  document.getElementById("warningPopup").classList.add("hidden");
+}
